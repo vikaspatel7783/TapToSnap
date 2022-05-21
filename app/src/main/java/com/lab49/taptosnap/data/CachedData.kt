2b62048a66f6e591710abda2b0b6babb08e74ada
Vikas Patel <vikaspatel7783@gmail.com>
2022-05-21 16:46:59 +0530

    Cached data utility added. Progressbar added for item list response

---
+++ app/src/main/java/com/lab49/taptosnap/data/CachedData.kt
@@ -0,0 +1,9 @@
+package com.lab49.taptosnap.data
+
+import com.lab49.taptosnap.model.Item
+
+object CachedData {
+
+    var itemList: List<Item> = emptyList()
+
+}
--- app/src/main/java/com/lab49/taptosnap/view/WelcomeActivity.kt
+++ app/src/main/java/com/lab49/taptosnap/view/WelcomeActivity.kt
@@ -2,28 +2,37 @@ package com.lab49.taptosnap.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.lab49.taptosnap.R
+import com.lab49.taptosnap.data.CachedData
 import com.lab49.taptosnap.data.ResultStatus
 import com.lab49.taptosnap.viewmodel.WelcomeViewModel
 
 class WelcomeActivity: AppCompatActivity() {
 
     private val welcomeViewModel: WelcomeViewModel by viewModels()
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_layout)
+        progressBar = findViewById(R.id.progressBar)
 
         findViewById<Button>(R.id.welcome_button_get_started).setOnClickListener {
 
+            progressBar.visibility = View.VISIBLE
+
             welcomeViewModel.getItemList().observe(this, { itemListResponse ->
+                progressBar.visibility = View.GONE
+
                 when (itemListResponse) {
                     is ResultStatus.Success -> {
-                        System.out.println(itemListResponse.data)
+                        CachedData.itemList = itemListResponse.data
                         startActivity(Intent(this, GameActivity::class.java));
                         finish()
                     }
